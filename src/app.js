812e8e3742d3e7ee46ffda25ca13fa17cca321ea
yurilinc-dev <yurilincoln.tech@gmail.com>
2023-09-19 00:50:54 -0300

    Commit automático

--- src/app.js
+++ src/app.js
@@ -5,11 +5,18 @@ app.use(express.json())
 
 //mock
 const selecoes = [
-    {id: 1, selecao: 'Brasil', grupo: 'G'},
-    {id: 2, selecao: 'Suiça', grupo: 'G'},
-    {id: 3, selecao: 'Sérvia', grupo: 'G'},
-    {id: 4, selecao: 'Camarões', grupo: 'G'},
+    { id: 1, selecao: 'Brasil', grupo: 'G' },
+    { id: 2, selecao: 'Suiça', grupo: 'G' },
+    { id: 3, selecao: 'Sérvia', grupo: 'G' },
+    { id: 4, selecao: 'Camarões', grupo: 'G' },
 ]
+
+function buscarSelecaoPorId(id){
+    return selecoes.filter(selecao => selecao.id == id)
+}
+function buscarIndexSelecao(id){
+    return selecoes.findIndex(selecao => selecao.id == id)
+}
 
 app.get('/', (req, res) => {
     res.send('Curso de NodeJS!')
@@ -17,9 +24,29 @@ app.get('/', (req, res) => {
 app.get('/selecoes', (req, res) => {
     res.status(200).send(selecoes)
 })
+app.get('/selecoes', (req, res) => {
+    res.status(200).send(selecoes)
+})
+app.get('/selecoes/:id', (req, res) => {
+    res.json(buscarSelecaoPorId(req.params.id))
+})
+
 app.post('/selecoes', (req, res) => {
     selecoes.push(req.body)
     res.status(201).send('Seleção cadastrada com sucesso!')
+})
+
+app.delete('/selecoes/:id', (req, res) => {
+    let index = buscarIndexSelecao(req.params.id)
+    selecoes.splice(index, 1)
+    res.send(`Seleção com id ${req.params.id} excluída com sucesso!`)
+})
+
+app.put('/selecoes/:id', (req, res) => {
+    let index = buscarIndexSelecao(req.params.id)
+    selecoes[index].selecao = req.body.selecao
+    selecoes[index].grupo   = req.body.grupo
+    res.json(selecoes)
 })
 
 
