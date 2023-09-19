46bc035f7f5f7de7f2c9c88008093a3c7d449247
yurilinc-dev <yurilincoln.tech@gmail.com>
2023-09-19 00:54:43 -0300

    Revert "Commit automático"
    This reverts commit 812e8e3742d3e7ee46ffda25ca13fa17cca321ea.

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 import app from './src/app.js'
-import connection from './infra/connection.js'
-
 const PORT = 3000
-connection.connect((error) => {
-    if (error) {
-        console.log(error)
-    } else {
-        console.log('\nConexão realizada com sucesso.\n')
 
-        app.listen(PORT, () => {
-            console.log(`A aplicação está rodando na url http://localhost:${PORT}.\nPara parar a aplicação pressione Ctrl + 'C'.`)
-        })
-    }
+app.listen(PORT, () => {
+console.log(`A aplicação está rodando na url http://localhost:${PORT}`)
 })
